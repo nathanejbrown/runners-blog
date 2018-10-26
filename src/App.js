949303d80bc6c460940ace79fce0b2ae6fabfa0b
Heathcliff <dennis.nathan@gmail.com>
2018-10-26 12:33:05 -0600

    Created floating box component, made minor styling change to header.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import FloatingBox from './components/FloatingBox/FloatingBox';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <FloatingBox />
       </div>
     );
   }
---
+++ src/components/FloatingBox/FloatingBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FloatingBox.css';
+
+const floatingBox = () => (
+    <React.Fragment>
+        <style>
+        @import url('https://fonts.googleapis.com/css?family=Mali');
+        </style>
+        <div className='box'>
+            <h1>It's about running and stuff.</h1>
+        </div>
+    </React.Fragment>
+)
+
+export default floatingBox;
