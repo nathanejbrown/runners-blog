e7820ddc3271d002b10c19a058a9a693f91238fa
Heathcliff <dennis.nathan@gmail.com>
2018-10-26 12:16:21 -0600

    Added a background to app.js, created Header component.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const header = () => (
+    <div className="header">
+        <h3>Running Blog Thing</h3>
+    </div>
+)
+
+export default header;
