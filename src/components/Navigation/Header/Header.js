61b3536e68ce93ef949641c741524834be38f979
Heathcliff <dennis.nathan@gmail.com>
2018-12-19 13:24:13 -0700

    Built a working side drawer that is only accessible on mobile.

---
+++ src/components/Navigation/Header/Header.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import './Header.css';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const header = (props) => {
+
+    return (
+        <Fragment>
+            <div className="header">
+                <div className='DrawerToggle' onClick={props.toggle}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <h3 className="headerH3"><a href="/">Running Blog Thing</a></h3>
+                <div className="innerHeader desktopOnly">
+                    <NavigationItems loggedIn={props.loggedIn} />
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default header;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+
+import NavigationItems from '../NavigationItems/NavigationItems';
+import './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+const sideDrawer = (props) => {
+  let attachedClasses = ['SideDrawer', 'Close'];
+  if (props.open) {
+    attachedClasses = ['SideDrawer', 'Open'];
+  }
+
+  return (
+    <Fragment>
+      <Backdrop show={props.open} clicked={props.toggle}/>
+      <div className={attachedClasses.join(' ')} onClick={props.toggle}>
+        <nav>
+          <NavigationItems loggedIn={props.loggedIn} />
+        </nav>
+      </div>
+    </Fragment>
+  )
+}
+
+export default sideDrawer;
