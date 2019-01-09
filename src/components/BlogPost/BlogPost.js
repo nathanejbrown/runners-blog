68db421aefce338391eb41661f8171deac226bc8
Heathcliff <dennis.nathan@gmail.com>
2019-01-09 12:13:59 -0700

    Setup blogPost to display spinner when loading.

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './BlogPost.css';
 
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 const blogPost = (props) => {
 
-    let postTitle = props.post ? props.post.title : 'No Blog';
-    let postBody = props.post ? props.post.body : 'Was found'
+    let post = null;
+
+    if (!props.loading && !props.post) {
+        post = (
+            <Fragment>
+                <h1 className='title'>No Blog</h1>
+                <p>Was found</p>
+            </Fragment>
+        )
+    } else {
+        post = (
+            <Fragment>
+                <h1 className='title'>{props.post.title}</h1>
+                <p>{props.post.body}</p>
+            </Fragment>
+        )
+    }
+
     let loadingAnimation = props.loading ? <Spinner /> : null
     return (
         <div className='post'>
             {loadingAnimation}
-            <h1 className='title'>{postTitle}</h1>
-            <p>{postBody}</p>
+            {post}
         </div>
     )
 }
