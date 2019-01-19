52d5cf3a897bffec7b553c7c914412f09e7e1509
Heathcliff <dennis.nathan@gmail.com>
2019-01-18 17:02:01 -0700

    Added buttons to switch between adding a new post and seeing all of your own posts on profile. Will style later.

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -17,7 +17,8 @@ class Profile extends Component {
             body: ''
         },
         token: null,
-        errorMessage: null
+        errorMessage: null,
+        display: 'all'
     }
     
     componentWillMount() {
@@ -46,6 +47,8 @@ class Profile extends Component {
         event.preventDefault();
         if (this.state.post.title && this.state.post.body) {
             this.props.createNewPost(this.state.post, this.state.token);
+            this.props.getAllPostsByAuthor(this.state.token);
+            this.setState({display: 'all'})
         } else {
             this.setState({errorMessage: 'Blog Post can\'t be empty'})
         }
@@ -53,6 +56,11 @@ class Profile extends Component {
 
     showPhotoUpload = () => {
         this.photoUpload.open();
+    }
+
+    allPosts = () => {
+        this.props.getAllPostsByAuthor(this.state.token);
+        this.setState({display: 'all'})
     }
     
     photoUpload = window.cloudinary.createUploadWidget({
@@ -70,8 +78,8 @@ class Profile extends Component {
     render () {
 
         let fullProfile = <Spinner />
-        let allPosts;
         let errorMessage = null;
+        let contentToDisplay = null;
 
         if (this.state.errorMessage) {
             errorMessage = (
@@ -84,40 +92,42 @@ class Profile extends Component {
             let formattedPosts = this.props.posts.map((post) => {
                 return <BlogPost post={post} loading={this.props.loading} key={post.id} />
             })
-            allPosts = (
-                <div className='postsContainer'>
+            if (this.state.display === 'all') {
+                contentToDisplay = (
+                    <div className='postsContainer'>
                     {formattedPosts}
                 </div>
-            )
+                )
+            } else {
+                contentToDisplay = (
+                    <div className='d-flex flex-column align-items-center'>
+                        <form>
+                            <div className='form-group col-sm-12'>
+                                <Input inputName='title' elementType='input' inputPlaceholder='Title' label='Title' changed={(event) => this.changedInput(event)}></Input>
+                            </div>
+                            <div className='form-group col-sm-12'>
+                                <Input inputName='body' elementType='input' inputPlaceholder='Body' label='Body' changed={(event) => this.changedInput(event)}></Input>
+                            </div>
+                            <div className='form-group col-sm-12'>
+                                <button type='button' className='btn btn-primary' onClick={(event) => this.sendNewPost(event)}>Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                )
+            }
             fullProfile = (
                 <Fragment>
                     <ProfileImage name={this.props.name} profileImageUrl={this.props.profileImageUrl} click={this.showPhotoUpload} />
                     <div className='profileContainer'>
                         {this.props.redirectPath}
                         {errorMessage}
-                        {allPosts}
-            <div className='d-flex flex-column align-items-center'>
-                <form>
-                    <div className='form-group col-sm-12'>
-                        <Input inputName='title' elementType='input' inputPlaceholder='Title' label='Title' changed={(event) => this.changedInput(event)}></Input>
-                    </div>
-                    <div className='form-group col-sm-12'>
-                        <Input inputName='body' elementType='input' inputPlaceholder='Body' label='Body' changed={(event) => this.changedInput(event)}></Input>
-                    </div>
-                    <div className='form-group col-sm-12'>
-                        <button type='button' className='btn btn-primary' onClick={(event) => this.sendNewPost(event)}>Submit</button>
-                    </div>
-                </form>
-            </div>
-                        {/* {formOrPost} */}
+                        <button onClick={this.allPosts}>All Posts</button>
+                        <button onClick={() => this.setState({display: 'new'})}>New Post</button>
+                        {contentToDisplay}
                     </div>
                 </Fragment>
             )
         }
-
-        // if (this.props.newPost) {
-        //     formOrPost = <BlogPost post={this.props.newPost} />
-        // }
 
         return (
             <Fragment>
